8844c9a5bb1c33c2d0ee60c4a9a52bfcb9ac481a
Chris O'Donnell <dev@codfish.io>
2019-09-09 12:56:58 -0400

    chore: babel build process

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * Test function.
+ *
+ * @param {boolean} maybe - Yea?
+ * @returns {string}
+ */
+export function testing(yea) {
+  return yea ? 'Yea' : 'No';
+}
+
+export default { testing };
