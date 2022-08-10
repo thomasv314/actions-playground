f19b77634e9907cc470e754c708685b6dd9f1385
Chris O'Donnell <dev@codfish.io>
2022-08-10 11:08:02 -0400

    feat: create react app for more testing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-/**
- * Test function
- *
- * @param {boolean} maybe - Yea?
- * @returns {string}
- */
-export function testing(yea) {
-  return yea ? 'Yesss' : 'No';
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-export default { testing };
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
